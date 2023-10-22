ebdd2a111084cafbe49b5eeab5964a373e21a011
Frank R. Haugen <frank.haugen@gmail.com>
2023-10-22 14:11:37 +0200

    Add GUI project based on Avalonia

---
+++ Frank.TorrentClient.Gui/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Frank.TorrentClient.Gui;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
